f75c384086955c26b6aa1d9c7bde60b9e10863f7
Orf1 <orfeas126@gmail.com>
2022-06-03 08:34:20 -0700

    Initial Commit

---
+++ Responsive-Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Responsive-Layout
+//
+//  Created by Orfeas Magoulas on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
